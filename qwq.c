3dc44567983c9521a5a4cebf175d877d51ccc3ff
sech1p <sech1p@disroot.org>
2021-11-03 20:18:11 +0100

    qwq qwq.c

---
+++ qwq.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+#define Q int
+#define w main(void)
+#define q {
+#define qwq if
+#define qwqwq (
+#define qwqwqwq write
+#define qwqwqwqwq )
+#define qwqwqwqwqwq ;
+#define qwqwqwqwqwqwq }
+#define qwqwqwqwqwqwqwq goto
+
+Q w q
+ qwq qwqwq qwqwqwq qwqwq 1, "q", 1 qwqwqwqwq qwqwqwqwq
+ q
+  QwQ: qwqwqwq qwqwq 1, "wq", 2 qwqwqwqwq qwqwqwqwqwq
+ qwqwqwqwqwqwq
+
+ qwqwqwqwqwqwqwq QwQ qwqwqwqwqwq /* we do a wittwe twowwing UwU */
+qwqwqwqwqwqwq
